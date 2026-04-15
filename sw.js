77652c58eecdc0b6a1914b51ec8166a0af9cf526
calycambe-max <calycambe@gmail.com>
2026-04-15 08:37:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
-self.addEventListener('install', e=>{
-e.waitUntil(
-caches.open('crm-cache').then(cache=>{
-return cache.addAll(['index.html','manifest.json']);
-})
-);
+const CACHE = "crm-pro-v1";
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(cache=>{
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', e=>{
-e.respondWith(
-caches.match(e.request).then(res=>res||fetch(e.request))
-);
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
+    })
+  );
 });
